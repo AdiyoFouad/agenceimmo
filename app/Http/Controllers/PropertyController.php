8c16d3578d5e0c1e1ba69363105253ee32dd4def
AdiyoFouad <odjfouad38@gmail.com>
2024-07-03 15:41:36 +0100

    Vue d'un bien

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,19 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SearchPropertiesRequest;
 use App\Models\Property;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
 {
-    public function index(){
-        $properties = Property::paginate(16);
+    public function index(SearchPropertiesRequest $request){
+
+        $query = Property::query()->orderBy('created_at', 'desc');
+
+        if($request->validated('price')){
+            $query = $query->where('price', '<=', $request->validated('price'));	
+        }
+        if($request->validated('surface')){
+            $query = $query->where('surface', '>=', $request->validated('surface'));	
+        }
+        if($request->validated('rooms')){
+            $query = $query->where('rooms', '>=', $request->validated('rooms'));	
+        }
+        if($request->validated('title')){
+            $query = $query->where('title', 'like', "%{$request->validated('title')}%");	
+        }
         return view("property.index", [
-            'properties' => $properties
+            'properties' => $query->paginate(16),
+            'input' => $request->validated()
         ]);
     }
 
     public function show(String $slug, Property $property){
-        dd($property);
+        if($slug != \Str::slug($property->title)){
+            return to_route('property.show', [
+                'slug'=> $slug,
+                'property' => $property
+            ]);
+        } else {
+            return view('property.show', [
+                'property' => $property
+            ]);
+        }
     }
 }
---
+++ resources/views/property/show.blade.php
@@ -0,0 +1,106 @@
+@extends('base')
+
+@section('title', $property->title)
+
+@section('content')
+
+    <div class="container">
+        
+
+        <div class="row">
+            <div class="mt-4 col-6">
+                <h1>{{ $property->title }}</h1>
+        <h2>{{ $property->rooms }} pièces - {{ $property->surface }}m²</h2>
+
+        <div class="text-primary fw-bold" style="font-size: 4rem;">
+            {{ number_format($property->price, thousands_separator : ' ') }} €
+        </div>
+
+        <hr>
+                <h4>Interressé par ce bien ?</h4>
+    
+                <form action="" method="POST" class="vstack gap-3" >
+                    @csrf
+                    <div class="row">
+                        @include('shared.input', [
+                            'label' => 'Nom',
+                            'name' => 'lastname',
+                            'class' => 'col'
+                        ])
+                        @include('shared.input', [
+                            'label' => 'Prénom',
+                            'name' => 'firstname',
+                            'class' => 'col'
+                        ])
+                    </div>
+                    <div class="row">
+                        @include('shared.input', [
+                            'label' => 'Téléphone',
+                            'name' => 'phone',
+                            'class' => 'col'
+                        ])
+                        @include('shared.input', [
+                            'label' => 'Email',
+                            'name' => 'firstname',
+                            'class' => 'col',
+                            'type' => 'email'
+                        ])
+                    </div>
+                    
+                    @include('shared.input', [
+                        'label' => 'Votre message',
+                        'name' => 'firstname',
+                        'class' => 'col',
+                        'type' => 'textarea'
+                    ])
+    
+                    <div>
+                        <button class="btn btn-primary">Nous contacter</button>
+                    </div>
+                </form>
+            </div>
+            <div class="mt-4 col-6">
+                <h2>Description</h2>
+                <p>{!! nl2br($property->description) !!}</p>
+                <div class="row">
+                    <div class="col-8">
+                        <h2>Caractéristique</h2>
+                        <table class="table table-striped">
+                            <tr>
+                                <td>Surface habitable</td>
+                                <td>{{ $property->surface }} m²</td>
+                            </tr>
+                            <tr>
+                                <td>Nbre de Pièce(s)</td>
+                                <td>{{ $property->rooms }} </td>
+                            </tr>
+                            <tr>
+                                <td>Nbre de Chambre(s)</td>
+                                <td>{{ $property->bedrooms }} </td>
+                            </tr>
+                            <tr>
+                                <td>Etage</td>
+                                <td>{{ $property->floor ?: 'Rez de chaussé' }} </td>
+                            </tr>
+                            <tr>
+                                <td>Localisation</td>
+                                <td>
+                                    {{ $property->address }} <br>
+                                    {{ $property->city }} ( {{ $property->postal_code }})
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="col-4">
+                        <h2>Spécificités</h2>
+                        <ul class="list-group">
+                            @foreach ($property->options as $option)
+                                <li class="list-group-item">{{$option->name}}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
